b18ccacf177629dda30081f6b75f1966ca7bb643
matheusnd <matheus.nery@gmail.com>
2019-01-12 14:04:24 -0200

    First commit

---
+++ wheaterPy.py
@@ -0,0 +1,34 @@
+from pprint import pprint
+import requests
+import json
+import datetime
+
+# Creating variables
+date_proc=datetime.date.today().strftime('%Y%m%d')
+APIKEY='318a6436abf2421287be1230935bfc99'
+fileCityID='city.list.json'
+fileResult='wheather_{}.json'.format(date_proc)
+
+def readCityID(country):
+    listCityID=[]
+    count=0
+    with open(fileCityID) as f:
+        data = json.load(f)
+        
+        for i in data:
+            if i['country'] == country:
+                # Max number of cityid per request
+                if count < 20:
+                    listCityID.append(i['id'])
+                    count+=1
+        
+    return(listCityID)                
+
+ids=','.join(str(e) for e in readCityID('DE'))
+
+url='http://api.openweathermap.org/data/2.5/group?id={}&units=metric&APPID={}'.format(ids,APIKEY)
+r = requests.get(url)
+dictRequest=r.json()
+
+with open(fileResult,'w') as f:
+    f.write(json.dumps(dictRequest))
